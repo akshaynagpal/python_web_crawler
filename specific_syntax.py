3abea1b211753ba8455a13518a8595dd5dbdb139
Akshay Nagpal <akshay2626@gmail.com>
2014-11-09 20:42:17 +0530

    Update specific_syntax.py

--- specific_syntax.py
+++ specific_syntax.py
@@ -38,3 +38,28 @@ Your choices are:
 3 salad
 4 nachos
 """
+
+"""
+zip FUNCTION (multiple lists)
+zip will create pairs of elements when passed two lists, and will stop at the end of the shorter list.
+zip can handle three or more lists as well!
+"""
+list_a = [3, 9, 17, 15, 19]
+list_b = [2, 4, 8, 10, 30, 40, 50, 60, 70, 80, 90]
+
+for a, b in zip(list_a, list_b):
+    # Add your code here!
+    if a>b:
+        print a
+    else:
+        print b
+        
+"""
+OUTPUT
+3
+9
+17
+15
+30
+"""
+"""
