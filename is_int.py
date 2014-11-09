e41396ae03c6173879def1f6d6225e48385e6f81
Akshay Nagpal <akshay2626@gmail.com>
2014-11-09 21:01:37 +0530

    Update is_int.py

--- is_int.py
+++ is_int.py
@@ -4,7 +4,8 @@ An integer is just a number without a decimal part (for instance, -17, 0, and 42
 For the purpose of this lesson, we'll also say that a number with a decimal part that is all 0s is also an integer, such as 7.0.
 This means that, for this lesson, you can't just test the input to see if it's of type int.
 
-Hint: If the difference between a number and that same number rounded down is greater than zero, what does that say about that particular number?
+Hint: If the difference between a number and that same number rounded down is greater than zero, what does that say about that 
+particular number?
 
 """
 #SOLUTION
