fd8df6bf75387dbc082018924493b2f476204664
Akshay Nagpal <akshay2626@gmail.com>
2014-12-12 17:08:20 +0530

    Update list_slice.py

--- list_slice.py
+++ list_slice.py
@@ -2,7 +2,9 @@
 List slicing allows us to access elements of a list in a concise manner. The syntax looks like this:
 
 [start:end:stride]
-Where start describes where the slice starts (inclusive), end is where it ends (exclusive), and stride describes the space between items in the sliced list. For example, a stride of 2 would select every other item from the original list to place in the sliced list.
+Where start describes where the slice starts (inclusive), end is where it ends (exclusive), and stride describes the space 
+between items in the sliced list. For example, a stride of 2 would select every other item from the original list to place 
+in the sliced list.
 """
 my_list = range(1, 11) # List of numbers 1 - 10
 
