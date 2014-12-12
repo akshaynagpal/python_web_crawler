013878dae0b2906730a1d6c12be30b6903bdedcf
Akshay Nagpal <akshay2626@gmail.com>
2014-12-12 17:19:09 +0530

    Update list_slice.py

--- list_slice.py
+++ list_slice.py
@@ -13,3 +13,14 @@ print my_list[::2]  #[1, 3, 5, 7, 9]
 backwards = my_list[::-1]
 
 print backwards  # [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
+
+
+to_21 = range(1,22)
+odds = to_21[::2]
+middle_third = to_21[7:14:1]
+
+print to_21         # [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21]
+
+print odds          # [1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21]
+
+print middle_third   #[8, 9, 10, 11, 12, 13, 14]
