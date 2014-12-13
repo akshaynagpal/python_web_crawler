08e3fde854fa362bbfa94bc18758db9ff22cf7e4
Akshay Nagpal <akshay2626@gmail.com>
2014-12-13 17:36:13 +0530

    Update insertion_problem.py
    added i=-1 case condition

--- insertion_problem.py
+++ insertion_problem.py
@@ -14,18 +14,20 @@ Sample Output
 2 4 4 6 8 
 2 3 4 6 8 
 """
-
 #!/bin/python
 def insertionSort(ar):  
     l = len(ar)
     ele = ar[l-1]
     i = l-2
-    while ele < ar[i]:
+    while ele < ar[i] and i>-1:
         ar[i+1] = ar[i]
         print str(ar).replace('[','').replace(']','').replace(',','')
         i -= 1
+    if i==-1:
+        ar[0]  = ele
+    else:
+        ar[i+1] = ele
     
-    ar[i+1] = ele
     print str(ar).replace('[','').replace(']','').replace(',','')
     return 1
 
