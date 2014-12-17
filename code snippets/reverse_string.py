4aa949edfce3f1a38100edfded60b94a2e238546
Akshay Nagpal <akshay2626@gmail.com>
2014-12-18 02:58:13 +0530

    updated

--- code snippets/reverse_string.py
+++
@@ -1,8 +0,0 @@
-# reverse a string in python without using reversed() or [::-1]
-
-def reverse(test):
-    n = len(test)
-    x=""
-    for i in range(n-1,-1,-1):
-        x += test[i]
-    return x
